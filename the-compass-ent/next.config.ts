754a0b6a7b84672841d840cef812f8beb099659f
The Compass Ent <thecompassent@gmail.com>
2026-01-04 20:16:11 +0900

    fix: simplify basePath to /webpage

--- the-compass-ent/next.config.ts
+++ the-compass-ent/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/webpage/the-compass-ent',
+  basePath: '/webpage',
   images: {
     unoptimized: true,
     remotePatterns: [
