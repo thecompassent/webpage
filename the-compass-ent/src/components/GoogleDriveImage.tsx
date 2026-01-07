6952ac353378b497fd969d62ac33060e3f00d99f
The Compass Ent <thecompassent@gmail.com>
2026-01-07 21:20:35 +0900

    fix: unoptimized images for events

--- the-compass-ent/src/components/GoogleDriveImage.tsx
+++ the-compass-ent/src/components/GoogleDriveImage.tsx
@@ -49,6 +49,7 @@ export default function GoogleDriveImage({
         <Image
             src={imageSrc}
             alt={alt}
+            unoptimized={true}
             {...props}
         />
     );
